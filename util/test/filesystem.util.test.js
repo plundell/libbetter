30cbd74a29741fcee2fc7afa672619740287f611
plundell <qmusicplayer@protonmail.com>
2020-05-19 16:56:47 +0200

    ...

--- util/test/filesystem.util.test.js
+++ util/test/filesystem.util.test.js
@@ -1,2 +1,7 @@
 // var fsX=require('./filesystem.util.js');
-var util=require('../util.node.js');
+var {BetterUtil:bu,BetterLog}=require('../../export-node.js');
+BetterLog.defaultOptions={autoPrintLvl:1};
+const log=new BetterLog('TEST');
+log.trace("Starting test of fsX...");
+let files=bu.fsX.ls('/home/buck/Documents/Software/Q/client/src/scripts/*.js',true);
+console.log(files);
